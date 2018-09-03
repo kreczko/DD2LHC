ab436aa7789000d00df648687abdd054f3681e03
kreczko <lkreczko@googlemail.com>
2018-09-03 15:38:33 +0100

    formatted app/views/main_views.py

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -6,7 +6,7 @@ from flask_user import current_user, login_required, roles_required
 from app import app, db, getUserPath
 from app.models.user_models import UserProfileForm
 
-#DM Limiter
+# DM Limiter
 from itertools import cycle
 from flask import Flask, render_template, request, redirect, url_for, session, json, make_response
 from werkzeug.utils import secure_filename
@@ -20,40 +20,45 @@ import pandas as pd
 from flask import send_from_directory
 import numpy as np
 #import pdfkit
-#DM Packages
+# DM Packages
 from app.dmplotter.plotter import get_data, get_datasets, get_metadata, set_SI_modifier, get_SI_modifier, getSimplifiedPlot, getDDPlot, getLegendPlot, validateFile
 from app.dmplotter.forms import DatasetForm, UploadForm, Set_gSM_Form
 from app.dmplotter.conversion import set_gSM, get_gSM
-#DM Default
+# DM Default
 ALLOWED_EXTENSIONS = set(['xml'])
 selected_datasets = []
 colors = cycle(['red', 'blue', 'green', 'orange'])
 
+
 def determine(data):
     if data is None:
-        return False;
+        return False
     else:
-        return True;
+        return True
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def getSavedPlots():
     globalPlots = json.load(open("savedPlots.json"))
-    userPlots  = {}
-    if(os.path.isfile('data/'+getUserPath()+'/userPlots.json')):
-        userPlots = json.load(open('data/'+getUserPath()+'/userPlots.json'))
+    userPlots = {}
+    if(os.path.isfile('data/' + getUserPath() + '/userPlots.json')):
+        userPlots = json.load(open('data/' + getUserPath() + '/userPlots.json'))
     allPlots = {**globalPlots, **userPlots}
     return allPlots
-#End DM
+# End DM
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 
 # The Home page is accessible to anyone
+
+
 @main_blueprint.route('/')
 def home_page():
-    #Re-direct to the plotter. The actual 'homepage' is a login/registration page
+    # Re-direct to the plotter. The actual 'homepage' is a login/registration page
     return redirect(url_for('main.dmplotter'))
 
 
@@ -92,13 +97,16 @@ def user_profile_page():
     return render_template('main/user_profile_page.html',
                            form=form)
 
+
 @main_blueprint.route('/about')
 def about_page():
     return render_template('about.html')
 
+
 @main_blueprint.route('/theory')
 def theory_page():
     return render_template('theory.html')
+
 
 @main_blueprint.route('/dmplotter', methods=['GET', 'POST'])
 def dmplotter():
@@ -106,7 +114,7 @@ def dmplotter():
     gu, gd, gs = get_gSM()
     si_modifier = get_SI_modifier()
 
-    #Generate Array of options
+    # Generate Array of options
     options = list(getSavedPlots().keys())
 
     dataset_selection = DatasetForm(gSM_input=gu)
@@ -124,7 +132,7 @@ def dmplotter():
         if dataset_selection.validate():
             selected_datasets = dataset_selection.datasets.data
             gSM = dataset_selection.gSM_input.data
-            set_gSM(gSM,gSM,gSM)
+            set_gSM(gSM, gSM, gSM)
             gu, gd, gs = get_gSM()
             si_modifier = dataset_selection.radio_inputSI.data
             set_SI_modifier(si_modifier)
@@ -132,12 +140,12 @@ def dmplotter():
     datasets = selected_datasets
     dfs = map(get_data, datasets)
 
-    #Filter, TODO: Apply filter to selected_datasets prior to conversion (attempt)
-    #get_data will return a 'None' object if the selected dataset could not be converted, (bad experiement string..)
+    # Filter, TODO: Apply filter to selected_datasets prior to conversion (attempt)
+    # get_data will return a 'None' object if the selected dataset could not be converted, (bad experiement string..)
     dfs = [x for x in dfs if determine(x)]
 
     metadata = map(get_metadata, datasets)
-    allmetadata = map (get_metadata,known_datasets)
+    allmetadata = map(get_metadata, known_datasets)
 
     p1 = getSimplifiedPlot()
     p2 = getDDPlot()
@@ -145,16 +153,16 @@ def dmplotter():
 
     legendItems = []
     x = 1
-    y = 2*x
+    y = 2 * x
     for df, color in zip(dfs, colors):
         label = df['label'].any()
         df['color'] = color
         p1.line(df['m_med'], df['m_DM'], line_width=2, color=color)
         p2.line(df['m_DM'], df['sigma'], line_width=2, color=color)
-        line = legendPlot.line(x,y,line_width=2, color=color)
-        legendItems.append((label,[line]))
+        line = legendPlot.line(x, y, line_width=2, color=color)
+        legendItems.append((label, [line]))
 
-    #Initialize all_data in the case that all datasets selected where invalid
+    # Initialize all_data in the case that all datasets selected where invalid
     all_data = pd.DataFrame()
     if(len(dfs) > 0):
         all_data = pd.concat(dfs)
@@ -164,36 +172,40 @@ def dmplotter():
 
     script1, div1 = components(p1, CDN)
     script2, div2 = components(p2, CDN)
-    script3, div3 = components(legendPlot,CDN)
-    return render_template('dmplotter.html',
-                           _debug=app.debug,_signedIn=current_user.is_authenticated,
-                           plot_script1=script1, plot_div1=div1,
-                           plot_script2=script2, plot_div2=div2,
-                           plot_script3=script3, plot_div3=div3,
-                           bokeh_version=bokeh.__version__,
-                           data_table=all_data.to_html(),
-                           datasets = known_datasets,
-                           metadata = metadata,
-                           dataset_selection = dataset_selection,
-                           selected_datasets = selected_datasets,
-                           allmetadata = allmetadata,
-                           savedPlots = getSavedPlots(),
-                           dataset_upload = dataset_upload,
-                           si_modifier = si_modifier,
-                           gSM_gSM=gu,
-                           gSM_gU=gu,gSM_gD=gd,gSM_gS=gs)
+    script3, div3 = components(legendPlot, CDN)
+    return render_template(
+        'dmplotter.html',
+        _debug=app.debug, _signedIn=current_user.is_authenticated,
+        plot_script1=script1, plot_div1=div1,
+        plot_script2=script2, plot_div2=div2,
+        plot_script3=script3, plot_div3=div3,
+        bokeh_version=bokeh.__version__,
+        data_table=all_data.to_html(),
+        datasets=known_datasets,
+        metadata=metadata,
+        dataset_selection=dataset_selection,
+        selected_datasets=selected_datasets,
+        allmetadata=allmetadata,
+        savedPlots=getSavedPlots(),
+        dataset_upload=dataset_upload,
+        si_modifier=si_modifier,
+        gSM_gSM=gu,
+        gSM_gU=gu, gSM_gD=gd, gSM_gS=gs,
+    )
+
 
 @main_blueprint.route('/updateValues', methods=['GET', 'POST'])
 def updateValues():
     gSM_refresh = Set_gSM_Form()
     gSM = gSM_refresh.gSM_input.data
-    set_gSM(gSM,gSM,gSM)
-    #Future: Individually set coupling constant
+    set_gSM(gSM, gSM, gSM)
+    # Future: Individually set coupling constant
     #gU = gSM_refresh.gU_input.data
     #gD = gSM_refresh.gD_input.data
     #gS = gSM_refresh.gS_input.data
-    #set_gSM(gU,gD,gS)
+    # set_gSM(gU,gD,gS)
     return redirect(url_for('main.dmplotter'))
+
 
 @main_blueprint.route('/savePlot', methods=['GET', 'POST'])
 @login_required
@@ -201,17 +213,18 @@ def savePlot():
     savedPlotName = request.form['name']
     selected_datasets = request.form['data'].split(",")
     userPlots = {}
-    #Create the User directory if not exists- TODO: do at time of account setup
-    directory = 'data/'+getUserPath()
+    # Create the User directory if not exists- TODO: do at time of account setup
+    directory = 'data/' + getUserPath()
     if not os.path.exists(directory):
         os.makedirs(directory)
-    if(os.path.isfile(directory+'/userPlots.json')):
-        userPlots = json.load(open('data/'+getUserPath()+'/userPlots.json'))
+    if(os.path.isfile(directory + '/userPlots.json')):
+        userPlots = json.load(open('data/' + getUserPath() + '/userPlots.json'))
     userPlots[savedPlotName] = selected_datasets
 
-    with open('data/'+getUserPath()+'/userPlots.json', 'w') as f:
+    with open('data/' + getUserPath() + '/userPlots.json', 'w') as f:
         json.dump(userPlots, f)
     return redirect(url_for('main.dmplotter'))
+
 
 @main_blueprint.route('/pdf', methods=['GET', 'POST'])
 def generatePDF():
@@ -220,16 +233,16 @@ def generatePDF():
     si_modifier = get_SI_modifier()
 
     global selected_datasets
-    #Will re-use the previously selected values for the dataset_type
-    #Optional: provide datasel file names in the POST parameters
+    # Will re-use the previously selected values for the dataset_type
+    # Optional: provide datasel file names in the POST parameters
     if request.method == 'POST':
-        print('Use this are to parse to posted datasets');
+        print('Use this are to parse to posted datasets')
 
     datasets = selected_datasets
     dfs = map(get_data, datasets)
 
-    #Filter, TODO: Apply filter to selected_datasets prior to conversion (attempt)
-    #get_data will return a 'None' object if the selected dataset could not be converted, (bad experiement string..)
+    # Filter, TODO: Apply filter to selected_datasets prior to conversion (attempt)
+    # get_data will return a 'None' object if the selected dataset could not be converted, (bad experiement string..)
     dfs = [x for x in dfs if determine(x)]
 
     metadata = map(get_metadata, datasets)
@@ -240,16 +253,16 @@ def generatePDF():
 
     legendItems = []
     x = 1
-    y = 2*x
+    y = 2 * x
     for df, color in zip(dfs, colors):
         label = df['label'].any()
         df['color'] = color
         p1.line(df['m_med'], df['m_DM'], line_width=2, color=color)
         p2.line(df['m_DM'], df['sigma'], line_width=2, color=color)
-        line = legendPlot.line(x,y,line_width=2, color=color)
-        legendItems.append((label,[line]))
+        line = legendPlot.line(x, y, line_width=2, color=color)
+        legendItems.append((label, [line]))
 
-    #Initialize all_data in the case that all datasets selected where invalid
+    # Initialize all_data in the case that all datasets selected where invalid
     all_data = pd.DataFrame()
     if(len(dfs) > 0):
         all_data = pd.concat(dfs)
@@ -259,16 +272,17 @@ def generatePDF():
 
     script1, div1 = components(p1, CDN)
     script2, div2 = components(p2, CDN)
-    script3, div3 = components(legendPlot,CDN)
-    html =  render_template('pdf.html',
-                           plot_script1=script1, plot_div1=div1,
-                           plot_script2=script2, plot_div2=div2,
-                           plot_script3=script3, plot_div3=div3,
-                           bokeh_version=bokeh.__version__,
-                           metadata = metadata,
-                           selected_datasets = selected_datasets,
-                           si_modifier = si_modifier,
-                           gSM_gSM=gu)
+    script3, div3 = components(legendPlot, CDN)
+    html = render_template(
+        'pdf.html',
+        plot_script1=script1, plot_div1=div1,
+        plot_script2=script2, plot_div2=div2,
+        plot_script3=script3, plot_div3=div3,
+        bokeh_version=bokeh.__version__,
+        metadata=metadata,
+        selected_datasets=selected_datasets,
+        si_modifier=si_modifier,
+        gSM_gSM=gu)
     return html
 '''
     css = 'app/static/css/style.css'
@@ -281,6 +295,7 @@ def generatePDF():
     return response
 '''
 
+
 @main_blueprint.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
@@ -288,14 +303,14 @@ def upload():
     if form.validate_on_submit():
         f = form.data_file.data
         filename = secure_filename(f.filename)
-        directory = os.path.join(app.config['UPLOAD_FOLDER'],getUserPath())
-        #Validate Data here
+        directory = os.path.join(app.config['UPLOAD_FOLDER'], getUserPath())
+        # Validate Data here
         if validateFile(f.read()):
             if not os.path.exists(directory):
                 os.makedirs(directory)
             f.seek(0)
             f.save(os.path.join(directory, filename))
-            flash(u'File uploaded.','info')
+            flash(u'File uploaded.', 'info')
         else:
-            flash(u'Could not parse input file- check required fields.','error')
+            flash(u'Could not parse input file- check required fields.', 'error')
     return redirect(url_for('main.dmplotter'))
